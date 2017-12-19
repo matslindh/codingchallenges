d8d1322cc701de765ab102a62437df5b0d1a5992
Mats Lindh <mats@lindh.no>
2017-12-19 10:15:26 +0100

    Solve Knowit2017/dec19

--- knowit2017/19.py
+++ knowit2017/19.py
@@ -5,7 +5,7 @@ x = y = 0
 instructions = open("input/dec19").readlines()
 
 for instr in instructions:
-    dist, dir = instr.strip().split(",")
+    dist, dir = instr.strip().split(", ")
     dx = 0
     dy = 0
     
@@ -21,16 +21,40 @@ for instr in instructions:
     if dir == 'north':
         dy = int(dist)
 
-    for ix in range(0, dx):
+    for ix in range(0, abs(dx)):
         if y not in buffer:
             buffer[y] = {}
             
-        x += 1
+        x += 1 if dx > 0 else -1
         buffer[y][x] = 1
 
-    for iy in range(0, dy):
+    for iy in range(0, abs(dy)):
+        y += 1 if dy > 0 else -1
+
         if y not in buffer:
             buffer[y] = {}
             
-        y += 1
         buffer[y][x] = 1
+
+    if y < low_y:
+        low_y = y
+
+    if y > high_y:
+        high_y = y
+
+    if x < low_x:
+        low_x = x
+
+    if x > high_x:
+        high_x = x
+
+
+for y in range(low_y, high_y + 1):
+    if y not in buffer:
+        print('')
+        continue
+
+    for x in range(low_x, high_x + 1):
+        print('x' if x in buffer[y] else ' ', end='')
+
+    print('')
