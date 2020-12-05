31aa18c3da1994f8ae5de15420ec80036293e35c
Mats Lindh <mats@lindh.no>
2020-12-05 21:47:14 +0100

    Solve Advent of Code 2020-12-05

---
+++ adventofcode2020/05.py
@@ -0,0 +1,30 @@
+def bitlify(s, true_char):
+    r = 0
+    l = len(s) - 1
+
+    for idx, c in enumerate(s):
+        r |= (1 if c == true_char else 0) << (l - idx)
+
+    return r
+
+
+def seat_indexer(s):
+    return bitlify(s[0:7], 'B') * 8 + bitlify(s[7:], 'R')
+
+
+def test_bitlify():
+    assert 44 == bitlify('FBFBBFF', 'B')
+    assert 5 == bitlify('RLR', 'R')
+
+
+def test_seat_indexer():
+    assert 357 == seat_indexer('FBFBBFFRLR')
+    assert 567 == seat_indexer('BFFFBBFRRR')
+    assert 119 == seat_indexer('FFFBBBFRRR')
+    assert 820 == seat_indexer('BBFFBBFRLL')
+
+
+if __name__ == '__main__':
+    all_ids = list(map(seat_indexer, [x.strip() for x in open('input/05').readlines()]))
+    print(max(all_ids))
+    print(set(range(0, 974)).symmetric_difference(set(all_ids)))
