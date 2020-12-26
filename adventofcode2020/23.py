bb3533f26e999fdf58d69cc38496294b17cf7f0e
Mats Lindh <mats@lindh.no>
2020-12-26 13:10:59 +0100

    Further tests and borked implementation of 23

--- adventofcode2020/23.py
+++ adventofcode2020/23.py
@@ -1,9 +1,32 @@
 def play(cups, iterations=100):
     idx = 0
     
-    while True:
+    while iterations:
+        current = cups[idx]
+        extracted = extract(cups, (idx + 1) % len(cups))
+        print(current, cups, extracted)
+        dest_idx = None
+
+        while True:
+            current -= 1
+            
+            if current < 1:
+                current = 9
+
+            try:
+                dest_idx = cups.index(current)
+                break
+            except ValueError:
+                continue
+        
+        cups = cups[:dest_idx+1] + extracted + cups[dest_idx+1:]
         idx = (idx + 1) % len(cups)
-    
+
+        print(cups)    
+        iterations -= 1
+
+    return cups
+
 
 def extract(cups, idx):
     end = (idx + 3) % len(cups)
@@ -24,6 +47,12 @@ def extract(cups, idx):
     return to_insert
 
 
+def order_after_one(cups):
+    idx = cups.index(1)
+    print(idx)
+    return cups[idx+1:] + cups[:idx]
+
+
 def test_extract():
     l = [1, 2, 3, 4, 5, 6]
     assert extract(l, 0) == [1, 2, 3]
@@ -37,4 +66,17 @@ def test_extract():
     assert extract(l, 4) == [5, 6, 1]
     assert l == [2, 3, 4]
 
-    
+
+def test_play():
+    cups = [int(x) for x in '389125467']
+    assert play(cups, 1) == [int(x) for x in '328915467']
+
+
+def test_order_after_one():
+    assert order_after_one([int(x) for x in '328915467']) == [int(x) for x in '54673289']
+
+
+def test_play_and_order():
+    cups = [int(x) for x in '389125467']    
+    assert order_after_one(play(cups, 9)) == [int(x) for x in '92658374']
+    assert order_after_one(play(cups)) == [int(x) for x in '67384529']
