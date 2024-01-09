6800c00d8fd5e41cd600b19462f67a01f3a74262
Mats Lindh <mats@lindh.no>
2024-01-09 21:45:25 +0100

    chore: utils-fix

--- adventofcode2023/utils.py
+++ adventofcode2023/utils.py
@@ -1,2 +1,6 @@
+import os.path
+
+
 def rs(path):
-    return open(f"input/{path}").read().splitlines()
+    base = os.path.dirname(__file__)
+    return open(f"{base}/input/{path}").read().splitlines()
