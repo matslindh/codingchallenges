6cbfbf4f12c94dd51fc874dc2d554fbab377a7a4
Mats Lindh <mats@lindh.no>
2024-12-15 00:17:38 +0100

    feat: common utility functions

---
+++ common/__init__.py
@@ -0,0 +1,13 @@
+def as_2d_ints(path):
+    return [
+        list(map(int, row))
+        for row in rs(path)
+    ]
+
+
+def as_1d_ints(path):
+    return list(map(int, rs(path)[0].split(' ')))
+
+
+def rs(path):
+    return open(path).read().splitlines()
