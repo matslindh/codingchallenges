3a5a497baf2aaa085eae0c48f45debe003ff263b
Mats Lindh <mats@lindh.no>
2025-12-02 10:59:38 +0100

    feat: knowit25/02 code example

---
+++ knowit2025/knowit02.py
@@ -0,0 +1,16 @@
+# solved manually, but lets code it as well
+from common import rs
+
+alphabet = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å',
+]
+
+for offset, line in enumerate(rs('input/02.txt')):
+    ans = ''
+
+    for char in line:
+        idx = alphabet.index(char) - offset - 1
+        ans += alphabet[idx]
+
+    print(ans)
